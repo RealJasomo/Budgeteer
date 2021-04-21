241b676705281c124a730b352d633ed9600a6e09
realjasomo <jason.cramer6@gmail.com>
2021-04-20 21:03:36 -0400

    Got authentication created

--- budgeteer-backend/src/resolvers.ts
+++ budgeteer-backend/src/resolvers.ts
@@ -1,5 +1,14 @@
 import { IResolvers } from "graphql-tools";
 
+import * as dotenv from 'dotenv';
+import * as bcrypt from "bcrypt";
+import * as EmailValidator from 'email-validator';
+import * as jwt from 'jsonwebtoken';
+
+import { User } from "./entity/User";
+
+dotenv.config();
+
 export const resolvers: IResolvers = {
     Query: {
         currentUser: (_, __, { req }) => {
@@ -8,7 +17,35 @@ export const resolvers: IResolvers = {
     },
     Mutation: {
         register: async (_, {email, password}) => {
+            var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+            var isPasswordStrong = strongRegex.test(password);
+            var isEmail = EmailValidator.validate(email);
+            if(!(isPasswordStrong || isEmail)){
+                return false;
+            }
+            var doesExist: User | undefined = await User.findOne({ email });
+            if(doesExist){
+                return false;
+            }
+            const hashedPassword = await bcrypt.hash(password, 14);
+            var user: User = await User.create({
+                email,
+                password: hashedPassword
+            }).save();
             return false;
+        },
+        login: async (_, {email, password}, { res }) => {
+            const user: User | undefined = await User.findOne({ email });
+            if(user){
+                const valid: boolean = await bcrypt.compare(password, user.password);
+                if(!valid){
+                    return null;
+                }
+                const token = jwt.sign({ userId: user.id }, process.env.TOKEN_SECRET as string);
+                res.cookie("token", token);
+                return user;
+            }
+            return null;
         }
     }
 }
